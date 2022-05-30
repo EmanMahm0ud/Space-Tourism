cdbd0652904e5e25d58c229084fada28b0a092a1
EmanMahm0ud <eman.talha2000@gmail.com>
2022-05-30 16:54:15 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import Nav from './Components/Nav';
 import Home from './Components/Home';
+import Destination from './Components/Destination';
+import Crew from './Components/Crew';
+import Technology from './Components/Technology';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Nav />
-      <Home />
-    </div>
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/destination' element={<Destination/>} />
+        <Route path='/crew' element={<Crew/>} />
+        <Route path='/technology' element={<Technology/>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import logopic from "../images/logo.png"
+import { Link } from "react-router-dom";
 
 function Nav() {
     return ( 
         <nav>
             <div className="logo"><img src={logopic} alt="logo"/></div>
             <ul>
-                <li><b>00</b> HOME</li>
-                <li><b>01</b> DESTINATION</li>
-                <li><b>02</b> CREW</li>
-                <li><b>03</b> TECHNOLOGY</li>
+                <Link to="/home"><li><b>00</b> HOME</li></Link>
+                <Link to="/destination"><li><b>01</b> DESTINATION</li></Link>
+                <Link to="/crew"><li><b>02</b> CREW</li></Link>
+                <Link to="technology"><li><b>03</b> TECHNOLOGY</li></Link>
             </ul>
         </nav>
     );

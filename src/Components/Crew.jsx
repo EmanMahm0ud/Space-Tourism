4378f8ac9aa482a94377f06275a6b8060dd9d052
EmanMahm0ud <eman.talha2000@gmail.com>
2022-08-04 00:01:40 +0200

    add crew slider

--- src/Components/Crew.jsx
+++ src/Components/Crew.jsx
@@ -1,23 +1,46 @@
+import { useState } from 'react';
 import data from '../Data/crew.json'
 import Staff from './Staff';
 
 function Crew() {
 
-    let crew = data.crew;
+    const [activeIndex, setActiveIndex] = useState(0);
+    const crew = data.crew;
     document.body.style.backgroundImage = `url(${require('../images/crew_bg.jpg')})`;
 
-    return ( 
+    const activeList = (key) => {
+        // to move the slider to the corresponding content
+        const slides = document.querySelector(".crew-slides");
+        slides.style.transform = `translateX(${key * -25}%)`;
+        setActiveIndex(key);
+
+        // to change the image source to the corresponding one
+        const image = document.querySelector('.crew img');
+        setTimeout(() => { image.style.opacity = '0'; })
+        setTimeout(() => { image.src = require(`../images/${crew[key].image_path}`); }, 200)
+        setTimeout(() => { image.style.opacity = '1'; }, 400)
+    }
+
+
+    return (
         <main className="crew">
             <div className="content">
                 <div className="title"><span className="page-num">02 </span>Meet your crew</div>
                 <section>
-                    <Staff name={crew[0].name} job={crew[0].job} description={crew[0].description}></Staff>
+                    <div className="crew-slider">
+                        <div className='crew-slides'>
+                            {crew.map((crew, i) =>
+                                <Staff key={i} name={crew.name} job={crew.job} description={crew.description}></Staff>
+                            )}
+                        </div>
+                    </div>
+
                 </section>
                 <div className="slider">
-                    <div className="circle"></div>
-                    <div className="circle"></div>
-                    <div className="circle"></div>
-                    <div className="circle"></div>
+                    <div onClick={() => activeList(0)} className={activeIndex === 0 ? "active-crew circle" : "circle"}></div>
+                    <div onClick={() => activeList(1)} className={activeIndex === 1 ? "active-crew circle" : "circle"}></div>
+                    <div onClick={() => activeList(2)} className={activeIndex === 2 ? "active-crew circle" : "circle"}></div>
+                    <div onClick={() => activeList(3)} className={activeIndex === 3 ? "active-crew circle" : "circle"}></div>
                 </div>
                 <div className="img-container"><img src={require("../images/crew1.png")} alt="crew" /></div>
             </div>
--- src/Components/Staff.jsx
+++ src/Components/Staff.jsx
@@ -1,6 +1,6 @@
 function Staff({name, job, description, i}) {
     return ( 
-        <div key={i}>
+        <div key={i} className="staff">
             <p className="job">{job}</p>
             <h2>{name}</h2>
             <p className="description">{description}</p>

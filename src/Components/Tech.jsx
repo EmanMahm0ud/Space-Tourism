5f56258c5c7c2efeea365cdfe04310f3d9a349d9
EmanMahm0ud <eman.talha2000@gmail.com>
2022-08-03 22:49:54 +0200

    add technology slider

--- src/Components/Tech.jsx
+++ src/Components/Tech.jsx
@@ -1,6 +1,6 @@
 function Tech({name, description, i}) {
     return ( 
-        <div key={i}>
+        <div key={i} className="tech">
             <h2>{name}</h2>
             <p className="description">{description}</p>
         </div>
--- src/Components/Technology.jsx
+++ src/Components/Technology.jsx
@@ -1,23 +1,45 @@
+import { useState } from 'react';
 import data from '../Data/technology.json'
 import Tech from './Tech';
 
 function Technology() {
 
-    let tech = data.technologies;
+    const [activeIndex, setActiveIndex] = useState(0);
+    const techs = data.technologies;
     document.body.style.backgroundImage = `url(${require('../images/technology_bg.jpg')})`;
 
-    return ( 
+    const activeList = (key) => {
+        // to move the slider to the corresponding content
+        const slides = document.querySelector(".tech-slides");
+        slides.style.transform = `translateY(${key * -33.333}%)`;
+        setActiveIndex(key);
+
+        // to change the image source to the corresponding one
+        const image = document.querySelector('.technology img');
+        setTimeout(() => { image.style.opacity = '0'; })
+        setTimeout(() => { image.src = require(`../images/${techs[key].image_path}`); }, 200)
+        setTimeout(() => { image.style.opacity = '1'; }, 400)
+    }
+
+    return (
         <main className="technology">
             <div className="content">
                 <div className="title"><span className="page-num">03 </span>SPACE LAUNCH 101</div>
                 <div className="slider">
-                    <div className="circle">1</div>
-                    <div className="circle">2</div>
-                    <div className="circle">3</div>
+                    <div onClick={() => activeList(0)} className={activeIndex === 0 ? "active-tech circle" : "circle"}>1</div>
+                    <div onClick={() => activeList(1)} className={activeIndex === 1 ? "active-tech circle" : "circle"}>2</div>
+                    <div onClick={() => activeList(2)} className={activeIndex === 2 ? "active-tech circle" : "circle"}>3</div>
                 </div>
                 <section>
                     <p className="sub-title">THE TERMINOLOGY…</p>
-                    <Tech name={tech[0].name} description={tech[0].description}></Tech>
+
+                    <div className="tech-slider">
+                        <div className='tech-slides'>
+                            {techs.map((tech, i) =>
+                                <Tech key={i} name={tech.name} description={tech.description}></Tech>
+                            )}
+                        </div>
+                    </div>
                 </section>
                 <img src={require("../images/technology1.jpg")} alt="technology" />
             </div>
